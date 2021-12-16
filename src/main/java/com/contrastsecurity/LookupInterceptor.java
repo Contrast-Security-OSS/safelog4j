1cd291a58269108b8011a8505cf07e1a60e5b9e0
Jeff Williams <planetlevel@gmail.com>
2021-12-16 15:28:54 -0500

    update to interceptor handling

--- src/main/java/com/contrastsecurity/LookupInterceptor.java
+++ src/main/java/com/contrastsecurity/LookupInterceptor.java
@@ -2,11 +2,14 @@ package com.contrastsecurity;
 
 import java.util.concurrent.Callable;
 
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import net.bytebuddy.implementation.bind.annotation.SuperCall;
 
 public class LookupInterceptor {
 
-    public static String intercept(@SuperCall Callable<String> zuper) throws Exception {
+    @RuntimeType
+    public static Object intercept(@AllArguments Object[] args, @SuperCall Callable<String> zuper) throws Exception {
 
         // is this a test log?
         if ( SafeLog4J.testScope.inScope() ) {
@@ -14,6 +17,12 @@ public class LookupInterceptor {
             return "";
         }
 
+        if ( SafeLog4J.blockMode ) {
+            return null;
+        }
+
         return zuper.call();
     }
 }
+
+  
